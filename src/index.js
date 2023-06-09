78b4210522da6acb88dd5df6eedf76f8354fc86c
skentala1 <135027565+skentala@users.noreply.github.com>
2023-06-09 11:22:48 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanillla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+populateTable();
+
+async function populateTable(){
+  const tableBody = document.getElementById("table-body");
+  const url = "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
+  const dataFetched = await fetch(url);
+  const dataJson = await dataFetched.json();
+  let municipalities = dataJson.dataset.dimension.Alue.category.label;
+  let populations = dataJson.dataset.value;
+  let keys = Object.keys(municipalities);
+  let i = 0;
+  keys.forEach((key) => {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    td1.innerText = municipalities[key];
+    console.log(municipalities[key]);
+    td2.innerText = populations[i++]
+    tr.appendChild(td1);   
+    tr.appendChild(td2);   
+    tableBody.appendChild(tr);
+  })
+}
